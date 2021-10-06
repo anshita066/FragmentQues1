8ff33954ad5196001f29304a47dce789abf7c15c
Anshita Sharma <anshita_066@yahoo.in>
2021-10-07 03:00:32 +0530

    Update SecondFragment.kt

--- app/src/main/java/com/example/fragmentques1/SecondFragment.kt
+++ app/src/main/java/com/example/fragmentques1/SecondFragment.kt
@@ -7,6 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 
 class SecondFragment : Fragment() {
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
