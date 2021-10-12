1caa822cb5f33ae9023abaa36ca2048c4a29df07
anshita066 <anshita_066@yahoo.in>
2021-10-12 13:55:24 +0530

    fragment life cycle log

--- app/src/main/java/com/example/fragmentques1/MainActivity.kt
+++ app/src/main/java/com/example/fragmentques1/MainActivity.kt
@@ -9,31 +9,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val firstFragment = FirstFragment()
-        val secondFragment = SecondFragment()
-
-        var btn1 = findViewById<View>(R.id.btnFragment1) as Button
-        var btn2 = findViewById<View>(R.id.btnFragment2) as Button
-
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.flfragment, firstFragment)
-            commit()
-        }
-
-        btn1.setOnClickListener{
-            supportFragmentManager.beginTransaction().apply {
-                replace(R.id.flfragment, firstFragment)
-                addToBackStack(null)
-                commit()
-            }
-        }
-        btn2.setOnClickListener{
-            supportFragmentManager.beginTransaction().apply {
-                replace(R.id.flfragment, secondFragment)
-                addToBackStack(null)
-                commit()
-            }
-        }
     }
 }
