8ec4e680fcca5ee3e6f908e667469487c809a9de
Anshita Sharma <anshita_066@yahoo.in>
2021-10-12 13:56:31 +0530

    Update FirstFragment.kt

--- app/src/main/java/com/example/fragmentques1/FirstFragment.kt
+++ app/src/main/java/com/example/fragmentques1/FirstFragment.kt
@@ -3,6 +3,34 @@ package com.example.fragmentques1
 import androidx.fragment.app.Fragment
 
 class FirstFragment : Fragment(R.layout.fragment_first) {
+   override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
 
+        Log.i("FRAGMENT1_LIFECYCLE", "onCreateView")
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onAttach(activity: Activity) {
+        super.onAttach(activity)
+
+        Log.i("FRAGMENT1_LIFECYCLE", "onAttach")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Log.i("FRAGMENT1_LIFECYCLE", "onCreate")
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        Log.i("FRAGMENT1_LIFECYCLE", "onActivityCreated")
+    }
 
     }
